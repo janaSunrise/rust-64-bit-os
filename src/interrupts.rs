20919c1d9e87f1648f1792e569bfe6594f95b907
janaSunrise <warriordefenderz@gmail.com>
2021-04-03 13:54:33 +0530

    Worked on Page interrupt handlers.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,6 @@
-use crate::{gdt, println, print};
+use crate::{gdt, println, print, hlt_loop};
 use lazy_static::lazy_static;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 use pic8259_simple::ChainedPics;
 use spin;
 
@@ -37,8 +37,11 @@ lazy_static! {
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
         }
+
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
         idt
     };
 }
@@ -101,3 +104,17 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(
         PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
 }
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
+}
+
